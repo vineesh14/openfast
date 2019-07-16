79baf7beade7cca6cbae67d4097c6686f0b4ad52
andrew-platt <andy.platt@nrel.gov>
2019-07-16 15:57:30 -0600

    FVW: move Pinv routine into routine that uses it.
    Remove MOD_MathOps module.  All its routines are in library or inline in the code.

--- modules/aerodyn14/src/Circulation.f90
+++ modules/aerodyn14/src/Circulation.f90
@@ -10,10 +10,12 @@ SUBROUTINE Calculate_Gamma1( n, VTotal, BladeTanVect, normalvector, BladeLoc, Co
       !       -- Description added by Kelsey Shaler
       ! ********************************************************
 
+  USE NWTC_Library
+  USE NWTC_LAPACK
+
   USE FVW_Parm
   USE AeroDyn14_Types, Only: FVW_WindType
   USE InflowWind
-  USE MathOps, Only: Pinv
 
 
   IMPLICIT NONE
@@ -141,6 +143,96 @@ SUBROUTINE Calculate_Gamma1( n, VTotal, BladeTanVect, normalvector, BladeLoc, Co
 
   Ainv = 0.0_ReKi
   VinducedNWFinal = 0.0_ReKi      !KS -- Why is this set to 0??
+
+CONTAINS
+   !=================================================
+   !> Calculate the inverse of the square matrix A using single value decomposition
+   !! routines in the LAPACK library
+   SUBROUTINE Pinv(A, M, Ainv, ErrStat, ErrMsg)
+
+      IMPLICIT NONE
+
+      INTEGER,                intent(in   )  :: M
+      REAL(ReKi),             intent(inout)  :: A(M,M)
+      REAL(ReKi),             intent(  out)  :: Ainv(M,M)
+      INTEGER(IntKi),         intent(  out)  :: ErrStat
+      CHARACTER(ErrMsgLen),   intent(  out)  :: ErrMsg
+
+      INTEGER(IntKi)                      :: lwork, lwmax
+      INTEGER(IntKi)                      :: r, summation, i
+
+      REAL( ReKi )                        :: tolerance
+      REAL( ReKi ),  ALLOCATABLE          :: WORK(:)
+      REAL( ReKi )                        :: S(M), U(M,M), VT(M,M), S_mat(M,M)
+
+      INTEGER(IntKi)                      :: ErrStat2
+      CHARACTER(ErrMsgLen)                :: ErrMsg2
+      CHARACTER(*),  PARAMETER            :: RoutineName='Pinv'
+
+      ErrStat = ErrID_None
+      ErrMsg  = ""
+
+
+   !FIXME: To optimize, setup the work array as a miscvar, calculate optimal size at init and set
+      !--------------------------
+      !  Find size of work array.
+      !--------------------------
+
+         ! set the size of the work array to something that we know will be possible to use
+      LWMAX = MIN(7*M,1000)
+      ALLOCATE(WORK(LWMAX))
+      work = 0.0_ReKi
+
+         ! Query the [d,s]gesvd LAPACK routines to find out the optimal size for the work array.
+      LWORK = -1
+      call LAPACK_gesvd('A', 'A', M, M, A, S, U, Vt, work, lwork, ErrStat2, ErrMsg2 )
+         call SetErrStat(ErrStat2,ErrMsg2,ErrStat,ErrMsg,RoutineName)
+
+         ! If LAPACK (or MKL) suggested a larger work array as optimal, reallocate work array.
+      if (LWMAX < work(1) ) then
+         LWMAX=work(1)
+         deallocate(work)
+         allocate(work(LWMAX))
+         work = 0.0_ReKi
+      endif
+
+      !--------------------------
+      !  Compute SVD.
+      !--------------------------
+
+      call LAPACK_gesvd('A', 'A', M, M, A, S, U, Vt, work, size(work), ErrStat2, ErrMsg2 )
+         call SetErrStat(ErrStat2,ErrMsg2,ErrStat,ErrMsg,RoutineName)
+
+         ! To speed up the calculations, find the tolerance and only calculate results
+         ! for terms above the tolerance
+      tolerance = M*epsilon(maxval(S))
+
+      summation=0
+      DO i=1,M
+         IF (s(i) .GT. tolerance) THEN
+            summation=summation+1;
+         END IF
+      END DO
+      r=summation
+
+         ! Set the diagonal elements of S_mat
+      S_mat = 0.0_ReKi
+      DO i = 1, M
+         IF (i .LE. r)THEN
+            S_mat(i,i)=1.0_ReKi/s(i)
+         END IF
+      END DO
+
+         ! Calculate the inverse of A
+      Ainv=transpose(matmul( matmul(U(:,1:r),S_mat(1:r,1:r)), VT(1:r,:)))
+
+      DEALLOCATE(WORK)
+
+   END SUBROUTINE Pinv
+   !=================================================
+
+
+
 
 END SUBROUTINE Calculate_Gamma1
 ! =====================================================================================
